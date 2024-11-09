71c34a2fdf707ef489f4c7e931d5fc7405329c58
A-Shermatov <azamatjon.shermatov@gmail.com>
2024-11-10 01:25:43 +0300

    Added images as resources

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -38,7 +38,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+)
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
